70f681cf8f5660aaad1bf658437eb307fb73df63
Ugur Unturk <ugurunturk@hotmail.com>
2013-11-12 23:14:06 +0200

    Name constraint modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
   #validates :name,  presence: true , length: { minimum:3 , maximum:50}
   validates :name, :presence => { message: "alanını boş bırakamazsınız!"} ,
-      length:{ minimum:5, maximum:20 , message:"minimum 5, maximum 20 karakterden oluşmalıdır."}
+      length:{ minimum:3, maximum:30 , message:"minimum 3, maximum 30 karakterden oluşmalıdır."}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, :presence => { message: "alanını boş bırakamazsınız!"}, 
       format: { with: VALID_EMAIL_REGEX , message:"formatı hatalı. Örnek: mail@example.com"}, uniqueness: { case_sensitive: false }
