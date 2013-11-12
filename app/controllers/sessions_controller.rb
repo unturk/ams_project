1d1b9b41c4b79fab45fadacfb07e0d764024e299
Ugur Unturk <ugurunturk@hotmail.com>
2013-11-12 23:06:06 +0200

    Change site/warning language to turkish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,12 @@ class SessionsController < ApplicationController
     
     if user && user.authenticate(params[:session][:password])
      sign_in user
-     #redirect_to user
-     redirect_back_or user
+     #redirect_back_or user
+     #redirect_back_or root_url
+     redirect_to root_url
+     flash[:success] = "Giriş başarılı!" 
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Hatalı email/şifre kombinasyonu, tekrar deneyiniz!'
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ActiveRecord::Base
   
   before_save { self.email = email.downcase }
-  #before_create :create_remember_token
-  validates :name,  presence: true , length: { minimum:3 , maximum:50}
-  #validates :name, :presence => { message: "alanını boş bırakamazsınız!"} , length:{ minimum:5, maximum:50}
+  before_create :create_remember_token
+  #validates :name,  presence: true , length: { minimum:3 , maximum:50}
+  validates :name, :presence => { message: "alanını boş bırakamazsınız!"} ,
+      length:{ minimum:5, maximum:20 , message:"minimum 5, maximum 20 karakterden oluşmalıdır."}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, :presence => { message: "alanını boş bırakamazsınız!"}, 
+      format: { with: VALID_EMAIL_REGEX , message:"formatı hatalı. Örnek: mail@example.com"}, uniqueness: { case_sensitive: false }
   
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password ,length: { minimum: 6 , maximum:15, message:"minimum 6, maximum 15 karakterden oluşmalıdır."}
   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
